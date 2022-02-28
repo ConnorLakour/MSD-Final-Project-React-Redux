4930e6b3ebee042e59e93a1bc705cd572aa7143d
Connor Lakour <lakour6@gmail.com>
2022-02-28 14:55:37 -0600

    initial

---
+++ frontend/client/src/Reducers/StreamReducers.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = {}, action) => {
+  switch (action.type) {
+    case "CREATE_STREAM":
+      //key interpolation       // find the stream id and assign action.payload
+      return { ...state, [action.payload.id]: action.payload };
+    case "FETCH_STREAM":
+      return { ...state, [action.payload.id]: action.payload };
+    case "FETCH_STREAMS":
+      return { ...state, [action.payload.id]: action.payload };
+    case "DELETE_STREAM":
+      return { ...state };
+    case "EDIT_STREAM":
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
